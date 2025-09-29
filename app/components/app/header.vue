da6ee6fb2ffa9cd2dbf9543b1ec66df305d91446
lone-DO <citizin@outlook.com>
2025-09-30 01:10:13 +0200

    AppHeader - Inject NavItem Dropdown
    - extrapolate Logo to reusable component

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
+import { NAVIGATION_ITEMS } from '~/lib/constants';
+</script>
+
 <template>
-  <header id="app-header" class="navbar bg-primary text-primary-content">
-    <div class="navbar-start">
-      <nuxt-link to="/" class="btn btn-ghost text-xl">
-        DAYSHAWN LOSOVOJ
-        <!-- INSERT NAME WITH NETFLIX FONT -->
+  <header id="app-header" class="navbar text-primary-content bg-base-300">
+    <div class="navbar-start items-center gap-2">
+      <nuxt-link to="/browse" class="p-0">
+        <AppLogo class="w-25" />
       </nuxt-link>
+      <AppDropdown :items="NAVIGATION_ITEMS" label="Browse" />
     </div>
     <div class="navbar-end">
       <AppThemeToggle />
