5fd816da78fb08eee243ec0faf931a49d671575f
lone-DO <citizin@outlook.com>
2025-10-08 22:38:10 +0200

    Rebuild SQL Migrations

--- app/lib/db/migrations/0001_melted_black_cat.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `profiles` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`profile_image_src` text,
-	`requires_auth` integer NOT NULL,
-	`created_at` integer NOT NULL,
-	`updated_at` integer NOT NULL
-);
--- app/lib/db/migrations/0002_magenta_cerise.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `skills` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`group` text NOT NULL,
-	`projects` text,
-	`image_src` text NOT NULL,
-	`created_at` integer NOT NULL,
-	`updated_at` integer NOT NULL
-);
