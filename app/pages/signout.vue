16eae779f5418e1cbc64ca08a68157edd01456d7
lone-do <citizin@outlook.com>
2026-07-12 10:49:08 -0400

    feat: Signout

--- app/pages/signout.vue
+++ app/pages/signout.vue
@@ -1,5 +1,9 @@
 <script lang='ts' setup>
+import { useAppStore } from '@/stores/app';
 /** TODO: OnMount, Logout of AppStore and redirect to /browse */
+const appStore = useAppStore();
+useRouter().replace('/');
+appStore.reset();
 </script>
 
 <template>
