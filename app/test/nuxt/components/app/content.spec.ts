3ed2e495243d2e61bc3e63e3aec5575529cdc5cf
lone-do <citizin@outlook.com>
2026-07-14 09:38:24 -0400

    patch: Latest Nuxt requires Typescript as dependency

--- app/test/nuxt/components/app/content.spec.ts
+++ app/test/nuxt/components/app/content.spec.ts
@@ -1,11 +1,11 @@
 import { mountSuspended } from '@nuxt/test-utils/runtime';
 import { describe, expect, it } from 'vitest';
 
-import Component from '~/components/app/content.vue';
+import Component from '~/components/content/list.vue';
 
 describe('appContent', () => {
   it('mounts', async () => {
     const wrapper = await mountSuspended(Component);
-    expect(wrapper.find('#content').exists()).toBeTruthy();
+    expect(wrapper.find('#content-list').exists()).toBeTruthy();
   });
 });
